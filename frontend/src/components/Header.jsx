fb0d4d501434ff557ca75b8b0f71f79f6cdcfb9d
Lukas Grimm <lukaskingsmail@gmail.com>
2020-08-16 13:42:30 +0200

    Created header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import pjson from '../../package.json';
+
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header class="header">
+      <h1>Web&shy;schnitt&shy;stellen - Ver&shy;gleich von ver&shy;schie&shy;denen Pro&shy;gram&shy;mier&shy;spra&shy;chen und Frame&shy;works zur Er&shy;stel&shy;lung von REST-Schnitts&shy;tellen</h1>
+      <div class="additional-info">
+        <b>Bachelorarbeit von <a href="https://lukasgrimm.me">Lukas Grimm</a></b>
+        <i>an der <a href="https://www.htw-berlin.de">Hochschule für Technik und Wirtschaft Berlin</a></i>
+        <span>Version: {pjson.version}</span>
+      </div>
+    </header>
+  )
+};
+
+export default Header;
