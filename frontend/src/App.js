5a0fe85a22b20193ba564962a52e085d738f524b
Lukas Grimm <lukaskingsmail@gmail.com>
2020-08-16 14:07:38 +0200

    Added routing to project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,17 @@ import React from 'react';
 import Header from './components/Header';
 
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/"></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
