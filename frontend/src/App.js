3eff79478c5599ddcad52f896f5840beb38490bc
Lukas Grimm <lukaskingsmail@gmail.com>
2020-08-16 14:54:48 +0200

    Created basic structure for App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 import Header from './components/Header';
+import Footer from './components/Footer';
+
+// Views
+import Toolbar from './views/Toolbar';
+import Blog from './views/Blog';
+import CreatePost from './views/CreatePost';
+import EditPost from './views/EditPost';
 
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
@@ -9,10 +16,20 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Toolbar />
       <BrowserRouter>
         <Switch>
-          <Route path="/"></Route>
+          <Route path="/post/create">
+            <CreatePost />
+          </Route>
+          <Route path="/post/edit/:postId">
+            <EditPost />
+          </Route>
+          <Route path="/">
+            <Blog />
+          </Route>
         </Switch>
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- frontend/src/views/EditPost.jsx
+++ frontend/src/views/EditPost.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router-dom'
 
 const EditPost = () => {
+  const { postId } = useParams();
+
   return (
-    <h1>Edit Post</h1>
+    <h1>Edit Post { postId }</h1>
   )
 };
 
