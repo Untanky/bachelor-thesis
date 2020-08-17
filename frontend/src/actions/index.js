18dcc083a0387bcae0c3533cdbb68069345803d8
Lukas Grimm <lukaskingsmail@gmail.com>
2020-08-17 18:05:31 +0200

    Extracted services to a configuration

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,12 +1,9 @@
+import ServiceConf from '../service.conf.json';
+
 export const selectLanguage = language => ({
   type: 'SELECT_LANGUAGE',
   language: LanguageSelection.find((item) => item.value === language),
 });
 
 
-export const LanguageSelection = [
-  { value: 'DEFAULT', text: 'Default (:8000)', port: 8000 },
-  { value: 'JAVA_SPRING', text: 'Java - Spring (:8080)', port: 8080 },
-  { value: 'JAVA_JAXRS', text: 'Java - JAXRS (:8081)', port: 8081 },
-  { value: 'CSHARP_ASPNET', text: 'C# - ASP.NET (:8082)', port: 8082 },
-]
+export const LanguageSelection = ServiceConf;
