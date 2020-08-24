72419ab6d042a615c833e8ee091ed7ef0b365228
Lukas Grimm <lukaskingsmail@gmail.com>
2020-08-24 12:02:36 +0200

    Initialized Spring framework

---
+++ backend/java/spring/src/main/java/me/lukasgrimm/bachelor/blog/spring/BachelorThesisSpringBlogApplication.java
@@ -0,0 +1,13 @@
+package me.lukasgrimm.bachelor.blog.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachelorThesisSpringBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BachelorThesisSpringBlogApplication.class, args);
+    }
+
+}
