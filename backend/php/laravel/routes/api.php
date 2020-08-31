a96af019ad760b88c59be3917e85eb8138bbb54c
Lukas Grimm <lukaskingsmail@gmail.com>
2020-08-31 16:25:49 +0200

    Extract closures

--- backend/php/laravel/routes/api.php
+++ backend/php/laravel/routes/api.php
@@ -35,59 +35,68 @@ $entityManager = EntityManager::create($conn, $config);
 
 $dao = new PostDBDAO($entityManager);
 
-Route::prefix('blog')->group(function() use ($dao) {
+$fetchAll = function() use ($dao) {
+    return $dao->findAll();
+};
+
+$createPost = function(Request $request) use ($dao) {
+    try {
+        $content = $request->getContent();
+        $decodedContent = json_decode($content);
+        $post = Post::fromArray($decodedContent);
+        $dao->create($post);
+        return 204;
+    } catch (IllegalArgumentException $e) {
+        return 400;
+    }
+};
+
+$updatePost = function ($id, Request $request) use ($dao) {
+    try {
+        $content = $request->getContent();
+        $decodedContent = json_decode($content);
+        $post = Post::fromArray($decodedContent);
+
+        if($post->getId() != $id) {
+            return 400;
+        }
+
+        $dao->update($post);
+        return 204;
+    } catch (IllegalArgumentException $e) {
+        return 404;
+    }
+};
+
+
+$deletePost = function($id) use ($dao) {
+    try {
+        $dao->delete($id);
+        return 204;
+    } catch (IllegalArgumentException $e) {
+        return 404;
+    }
+};
+
+Route::prefix('blog')->group(function() use ($fetchAll, $createPost, $updatePost, $deletePost) {
 
     /**
      * Display All Posts
      */
-    Route::get('/post', function () use ($dao) {
-        return $dao->findAll();
-    });
+    Route::get('/post', $fetchAll);
 
     /**
      * Add A New Post
      */
-    Route::post('/post', function (Request $request) use ($dao) {
-        try {
-            $content = $request->getContent();
-            $decodedContent = json_decode($content);
-            $post = Post::fromArray($decodedContent);
-            $dao->create($post);
-            return 204;
-        } catch (IllegalArgumentException $e) {
-            return 400;
-        }
-    });
+    Route::post('/post', $createPost);
 
     /**
      * Update A Post
      */
-    Route::put('/post/{id}', function ($id, Request $request) use ($dao) {
-        try {
-            $content = $request->getContent();
-            $decodedContent = json_decode($content);
-            $post = Post::fromArray($decodedContent);
-
-            if($post->getId() != $id) {
-                return 400;
-            }
-
-            $dao->update($post);
-            return 204;
-        } catch (IllegalArgumentException $e) {
-            return 404;
-        }
-    });
+    Route::put('/post/{id}', $updatePost);
 
     /**
      * Delete An Existing Post
      */
-    Route::delete('/post/{id}', function ($id) use ($dao) {
-        try {
-            $dao->delete($id);
-            return 204;
-        } catch (IllegalArgumentException $e) {
-            return 404;
-        }
-    });
+    Route::delete('/post/{id}', $deletePost);
 });
