75d09533e2d4445ab8ed3f9b9974d09a3de1d846
Lukas Grimm <lukaskingsmail@gmail.com>
2020-08-31 15:24:46 +0200

    Implemented routes for laravel implementation

--- backend/php/dao/src/model/Post.php
+++ backend/php/dao/src/model/Post.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity @ORM\Table(name="post")
  */
-class Post
+class Post implements \JsonSerializable
 {
     /** @ORM\ID @ORM\Column(type="integer") @ORM\GeneratedValue */
     private $id;
@@ -17,6 +17,24 @@ class Post
 
     /** @ORM\Column(type="string") */
     private $description;
+
+    public function __construct()
+    {
+    }
+
+    public static function fromArray($data) {
+        $post = new Post();
+        if(property_exists($data, 'id')) {
+            $post->id = $data->id;
+        }
+        if(property_exists($data, 'title')) {
+            $post->title = $data->title;
+        }
+        if(property_exists($data, 'description')) {
+            $post->description = $data->description;
+        }
+        return $post;
+    }
 
     /**
      * @return mixed
@@ -56,5 +74,20 @@ class Post
     public function setDescription($description)
     {
         $this->description = $description;
+    }
+
+    public function getUrl() {
+        return "/api/blog/post/" . $this->getId();
+    }
+
+    public function jsonSerialize()
+    {
+        return
+            [
+                'id'   => $this->getId(),
+                'title' => $this->getTitle(),
+                'description' => $this->getDescription(),
+                'url' => $this->getUrl()
+            ];
     }
 }
--- backend/php/laravel/routes/api.php
+++ backend/php/laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use exception\IllegalArgumentException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,32 +15,79 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('blog')->group(function() {
+use dao\PostDBDAO;
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+use model\Post;
+
+// Create a simple "default" Doctrine ORM configuration for Annotations
+$isDevMode = true;
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."../dao/src"), $isDevMode, null, null, false);
+// or if you prefer XML
+// $config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config"), $isDevMode);
+// database configuration parameters
+$conn = array(
+    'url' => 'pdo-pgsql://root:root@localhost:5432/blog'
+);
+
+// obtaining the entity manager
+$entityManager = EntityManager::create($conn, $config);
+
+$dao = new PostDBDAO($entityManager);
+
+Route::prefix('blog')->group(function() use ($dao) {
+
     /**
      * Display All Posts
      */
-    Route::get('/post', function () {
-        //
+    Route::get('/post', function () use ($dao) {
+        return $dao->findAll();
     });
 
     /**
      * Add A New Post
      */
-    Route::post('/post', function (Request $request) {
-        //
+    Route::post('/post', function (Request $request) use ($dao) {
+        try {
+            $content = $request->getContent();
+            $decodedContent = json_decode($content);
+            $post = Post::fromArray($decodedContent);
+            $dao->create($post);
+            return 204;
+        } catch (IllegalArgumentException $e) {
+            return 400;
+        }
     });
 
     /**
      * Update A Post
      */
-    Route::put('/post/{id}', function ($id, Request $request) {
-        //
+    Route::put('/post/{id}', function ($id, Request $request) use ($dao) {
+        try {
+            $content = $request->getContent();
+            $decodedContent = json_decode($content);
+            $post = Post::fromArray($decodedContent);
+
+            if($post->getId() != $id) {
+                return 400;
+            }
+
+            $dao->update($post);
+            return 204;
+        } catch (IllegalArgumentException $e) {
+            return 404;
+        }
     });
 
     /**
      * Delete An Existing Post
      */
-    Route::delete('/task/{id}', function ($id) {
-        //
+    Route::delete('/post/{id}', function ($id) use ($dao) {
+        try {
+            $dao->delete($id);
+            return 204;
+        } catch (IllegalArgumentException $e) {
+            return 404;
+        }
     });
 });
