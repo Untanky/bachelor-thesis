0ecec6bd8ca94db152db469ca16f48a91cfc3f24
Lukas Grimm <lukaskingsmail@gmail.com>
2020-09-07 11:39:09 +0200

    Implemented PostController with unit tests

--- backend/php/laravel/app/Providers/AppServiceProvider.php
+++ backend/php/laravel/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind('dao\PostDAO', function ($app) {
+            // Create a simple "default" Doctrine ORM configuration for Annotations
+            $isDevMode = true;
+            $config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src"), $isDevMode, null, null, false);
+            // or if you prefer XML
+            // $config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config"), $isDevMode);
+            // database configuration parameters
+            $conn = array(
+                'url' => 'pdo-pgsql://root:root@localhost:5432/blog'
+            );
+
+            // obtaining the entity manager
+            $entityManager = EntityManager::create($conn, $config);
+
+            return new \dao\PostDBDAO($entityManager);
+        });
     }
 
     /**
--- backend/php/laravel/routes/api.php
+++ backend/php/laravel/routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use exception\IllegalArgumentException;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,88 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-use dao\PostDBDAO;
-use Doctrine\ORM\Tools\Setup;
-use Doctrine\ORM\EntityManager;
-use model\Post;
-
-// Create a simple "default" Doctrine ORM configuration for Annotations
-$isDevMode = true;
-$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."../dao/src"), $isDevMode, null, null, false);
-// or if you prefer XML
-// $config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config"), $isDevMode);
-// database configuration parameters
-$conn = array(
-    'url' => 'pdo-pgsql://root:root@localhost:5432/blog'
-);
-
-// obtaining the entity manager
-$entityManager = EntityManager::create($conn, $config);
-
-$dao = new PostDBDAO($entityManager);
-
-$fetchAll = function() use ($dao) {
-    return $dao->findAll();
-};
-
-$createPost = function(Request $request) use ($dao) {
-    try {
-        $content = $request->getContent();
-        $decodedContent = json_decode($content);
-        $post = Post::fromArray($decodedContent);
-        $dao->create($post);
-        return 204;
-    } catch (IllegalArgumentException $e) {
-        return 400;
-    }
-};
-
-$updatePost = function ($id, Request $request) use ($dao) {
-    try {
-        $content = $request->getContent();
-        $decodedContent = json_decode($content);
-        $post = Post::fromArray($decodedContent);
-
-        if($post->getId() != $id) {
-            return 400;
-        }
-
-        $dao->update($post);
-        return 204;
-    } catch (IllegalArgumentException $e) {
-        return 404;
-    }
-};
-
-
-$deletePost = function($id) use ($dao) {
-    try {
-        $dao->delete($id);
-        return 204;
-    } catch (IllegalArgumentException $e) {
-        return 404;
-    }
-};
-
-Route::prefix('blog')->group(function() use ($fetchAll, $createPost, $updatePost, $deletePost) {
+Route::prefix('blog')->group(function() {
 
     /**
      * Display All Posts
      */
-    Route::get('/post', $fetchAll);
+    Route::get('/post', 'PostController@fetchAll');
 
     /**
      * Add A New Post
      */
-    Route::post('/post', $createPost);
+    Route::post('/post', 'PostController@createPost');
 
     /**
      * Update A Post
      */
-    Route::put('/post/{id}', $updatePost);
+    Route::put('/post/{id}', 'PostController@updatePost');
 
     /**
      * Delete An Existing Post
      */
-    Route::delete('/post/{id}', $deletePost);
+    Route::delete('/post/{id}', 'PostController@deletePost');
 });
