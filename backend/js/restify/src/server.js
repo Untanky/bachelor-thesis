9bba1a2eba24f608ac56dda1fdcb3c65b0093aab
Lukas Grimm <lukaskingsmail@gmail.com>
2020-09-14 22:06:20 +0200

    Implemented restify

--- backend/js/restify/src/server.js
+++ backend/js/restify/src/server.js
@@ -16,32 +16,37 @@ server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
-router(server, true)([
-  {
-    group: 'api/blog',
-    routes: [
-      {
-        match: 'post',
-        method: 'get',
-        action: fetchAllPosts,
-      },
-      {
-        match: 'post',
-        method: 'post',
-        action: createPost,
-      },
-      {
-        match: 'post/:postId',
-        method: 'put',
-        action: updatePost,
-      },
-      {
-        match: 'post/:postId',
-        method: 'delete',
-        action: deletePost,
-      },
-    ],
-  },
-]);
+server.get('/api/blog/post', fetchAllPosts);
+server.post('/api/blog/post', createPost);
+server.put('/api/blog/post/:postId', updatePost);
+server.del('/api/blog/post/:postId', deletePost);
+
+// router(server, true)([
+//   {
+//     group: 'api/blog/post',
+//     routes: [
+//       {
+//         match: ':postId',
+//         method: 'put',
+//         action: updatePost,
+//       },
+//       {
+//         match: ':postId',
+//         method: 'delete',
+//         action: deletePost,
+//       },
+//       {
+//         match: '',
+//         method: 'get',
+//         action: fetchAllPosts,
+//       },
+//       {
+//         match: '',
+//         method: 'post',
+//         action: createPost,
+//       },
+//     ],
+//   },
+// ]);
 
 export default server;
