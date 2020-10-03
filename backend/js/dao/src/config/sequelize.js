c7f568308b83f4758e267c2f60901cd2be07a12d
Lukas Grimm <lukaskingsmail@gmail.com>
2020-10-03 14:59:57 +0200

    Added more files

--- backend/js/dao/src/config/sequelize.js
+++ backend/js/dao/src/config/sequelize.js
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('postgresql://root:root@localhost:5432/blog');
+const sequelize = new Sequelize('postgresql://root:root@database:5432/blog');
 
 export default sequelize;
