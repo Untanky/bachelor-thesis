2655a56d8dd6a891062a1433beaa3adee22b4ad9
Lukas Grimm <lukaskingsmail@gmail.com>
2020-10-25 09:45:35 +0100

    Refactored handler further

--- backend/python/django/rest/blog/views.py
+++ backend/python/django/rest/blog/views.py
@@ -1,16 +1,23 @@
 from rest_framework.decorators import api_view
-from handlers import fetchAllPosts, createPost, updatePost, deletePost
+from sqlalchemy import create_engine
+from handlers import PostController
+import sys, os
+sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/../../../dao/src"))
+from PostDAO import PostDAO
+
+postDAO = PostDAO(create_engine("postgres://root:root@localhost:5432/blog"))
+postController = PostController(postDAO)
 
 @api_view(['GET', 'POST'])
 def post(request):
     if request.method == 'GET':
-        return fetchAllPosts(request)
+        return postController.fetchAllPosts(request)
     if request.method == 'POST':
-        return createPost(request)
+        return postController.createPost(request)
 
 @api_view(['PUT', 'DELETE'])
 def post_by_id(request, id):
     if request.method == 'PUT':
-        return updatePost(id, request)
+        return postController.updatePost(id, request)
     if request.method == 'DELETE':
-        return deletePost(id, request)
+        return postController.deletePost(id, request)
