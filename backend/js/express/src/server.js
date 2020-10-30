358d7fa41e47db29a68e367195d7268cda35e4a5
Lukas Grimm <lukaskingsmail@gmail.com>
2020-10-30 11:18:40 +0100

    Added cors to express app

--- backend/js/express/src/server.js
+++ backend/js/express/src/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import blog from './blog';
 
 const server = express();
 
 server.use(bodyParser.json());
+server.use(cors({ origin: '*' }));
 
 server.use('/api/blog', blog);
 
