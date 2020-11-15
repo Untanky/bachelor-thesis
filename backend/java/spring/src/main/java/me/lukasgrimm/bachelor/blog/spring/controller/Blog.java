1a5651099841bd8fce47bc3ee821afb57343280c
Lukas Grimm <lukaskingsmail@gmail.com>
2020-11-15 17:46:58 +0100

    Containerized Frontend

--- backend/java/spring/src/main/java/me/lukasgrimm/bachelor/blog/spring/controller/Blog.java
+++ backend/java/spring/src/main/java/me/lukasgrimm/bachelor/blog/spring/controller/Blog.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/blog")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class Blog {
 
     PostDAO dao;
